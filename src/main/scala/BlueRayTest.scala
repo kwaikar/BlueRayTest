b6b48f5547e575b175ff66e6a6f0a0ebc5267760
kwaikar <wkanchan@gmail.com>
2017-02-24 23:29:26 -0600

    commit

--- src/main/scala/BlueRayTest.scala
+++ src/main/scala/BlueRayTest.scala
@@ -8,25 +8,31 @@ object BlueRayTest {
     val conf = new SparkConf().setAppName("BlueRay Test Application").setMaster(args(3));
     val sc = SparkContext.getOrCreate(conf)
     sc.setLocalProperty(("USER"), args(0));
-    var inputFile = sc.textFile(args(1))
- println("done" + inputFile.count())
-    //inputFile.collect().foreach(println)
- // println("done" + inputFile.collect().mkString(","))
-    
- sc.setLogLevel("DEBUG");
-    val stringToBeBlocked="White"
-    val valueNotBlocked="Black"
-    val newString="ABC";
-    val currentMillis = System.currentTimeMillis;
-    println(inputFile.collect().size + " =======> " + inputFile.collect().mkString)
-    println(inputFile.collect().mkString.contains(stringToBeBlocked))
-    println(inputFile.collect().mkString.contains(valueNotBlocked))
-    println("#$%#$%" + inputFile.collect().mkString)
-    println(inputFile.collect().mkString.contains(newString))
-   print(3 == inputFile.count());
-    println(inputFile.take(3).mkString.contains(newString))
-    println(inputFile.takeSample(false, 3, 0).mkString.contains(newString)) 
-    println(inputFile.first().toString().contains(newString))
 
+      var inputFile = sc.textFile(args(1))
+
+    val fs = org.apache.hadoop.fs.FileSystem.get(new java.net.URI(args(2)), sc.hadoopConfiguration)
+    if (fs.exists(new org.apache.hadoop.fs.Path(args(2)))) {
+      fs.delete(new org.apache.hadoop.fs.Path(args(2)));
+    }
+    /*inputFile.saveAsTextFile(args(2)) val df = sqlContext.read.schema(customSchema).csv(args(1))
+    df.groupBy("age").count().show()  */
+    /* val customSchema = StructType(Array(
+      StructField("gender", StringType, true),
+      StructField("zip", StringType, true),
+      StructField("age", StringType, true),
+      StructField("race", StringType, true)))
+
+    val sqlContext = new SQLContext(sc)
+    val df = sqlContext.read.schema(customSchema).csv(args(1))
+    df.groupBy("age").count().show()
+*/
+
+  /*  val values = inputFile.flatMap(_.split("\n")).map(_.split(",")).map(x => ((x(2), x(3)), 1));
+    values.reduceByKey(_+_).take(5).foreach(println);
+  */
+    println("Count : ::"+inputFile.flatMap(_.split("\n")).count());
+      
   }
 }
+
